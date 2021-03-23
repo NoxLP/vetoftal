8b207e2588d5c7d98e2bc8ebb7b3b2eba27e565f
Norber <nox_nox_@hotmail.com>
2021-03-23 01:21:13 +0000

    Fixes to header. Added first home text

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <v-app-bar dense fixed color="white" shrink-on-scroll app class="pa-0">
+    <v-app-bar
+      height="100vh"
+      fixed
+      color="white"
+      :shrink-on-scroll="$vuetify.breakpoint.smAndDown"
+      app
+      class="pa-0"
+    >
       <v-app-bar-nav-icon
         fixed
         class="ma-auto"
@@ -14,18 +21,23 @@
         style="height: 80%"
       ></v-img>
       <v-container v-if="$vuetify.breakpoint.mdAndUp" full-height fluid>
-        <v-row justify="start" align="center">
-          <v-col cols="2" align="start" class="ma-0 mt-2 pa-0">
+        <v-row justify="start" align="center" class="pb-lg-0 pt-lg-0">
+          <v-col cols="2" lg="1" align="start" class="ma-0 mt-2 mt-lg-0 pa-0">
             <v-img
+              v-if="$vuetify.breakpoint.md"
               src="@/assets/images/LogoNewPeqWhite.png"
               contain
-              height="100"
-              width="200"
+              height="80%"
             ></v-img>
+            <img
+              v-else
+              src="@/assets/images/LogoNewPeqWhite.png"
+              class="ma-0 pa-0"
+            />
           </v-col>
           <v-spacer></v-spacer>
           <v-col>
-            <v-tabs active-class="active-tabs">
+            <v-tabs class="my-auto" active-class="active-tabs">
               <v-tab
                 v-for="(tab, idx) in tabs"
                 :key="idx"
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="home">
     <Carousel />
-    <v-container class="mt-16">
-      <v-row>
-        <v-col cols="12" md="6">
+    <v-container class="mt-16" fluid>
+      <v-row align="center">
+        <v-col cols="12" md="4" offset-md="2">
           <v-card elevation="8">
             <v-img
               src="@/assets/images/home/sarandy-westfall--itLKdE7ojA-unsplash.jpg"
             >
             </v-img>
           </v-card>
+        </v-col>
+        <v-col cols="12" md="4" class="ml-md-16">
+          <p
+            class="text-justify text-body-1 text-md-h5"
+            style="white-space: pre-line"
+          >
+            Nuestra experiencia nos avala, diez años ejerciendo la profesión son
+            una garantía de profesionalidad y buen hacer.
+          </p>
+          <p
+            class="text-justify text-body-1 text-md-h5"
+            style="white-space: pre-line"
+          >
+            Una formación continua nos prepara para afrontar cualquier
+            contingencia, siempre pensando en el bienestar de nuestras queridas
+            mascotas.
+          </p>
         </v-col>
       </v-row>
       <v-row> </v-row>
@@ -28,7 +45,6 @@ export default {
 </script>
 <style scoped>
 .background-sec-trans {
-  /*box-shadow: #d9ffe38e !important;*/
   background: #966dd09f !important;
 }
 .border-red {
