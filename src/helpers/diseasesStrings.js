fb847716a0ffca7d869f9fd12b20ee2a57fc4f0b
Norber <nox_nox_@hotmail.com>
2021-03-24 23:15:27 +0000

    Diseases strings and mobile diseases page

--- src/helpers/diseasesStrings.js
+++ src/helpers/diseasesStrings.js
@@ -1,32 +1,8 @@
-const DISEASES = [
-  {
-    title: 'Queratoconjuntivitis seca',
-    paragraphs: [
-      'Esta alteración es muy frecuente en ciertas razas caninas como el Yorkshire Terrier, el West Highland White Terrier y el Bulldog Francés entre otros. Se caracteriza por una disminución de la producción de uno o varios componentes de la película lagrimal.',
-      'Las mascotas que padecen esta enfermedad suelen presentar legañas, conjuntivitis recurrentes y úlceras corneales frecuentes.',
-      'Esta alteración es crónica, pero en muchas ocasiones si se diagnostica a tiempo, las mascotas que lo padecen se pueden controlar con un tratamiento y ciertos cuidados determinados.',
-      'Se recomienda acudir al oftalmólogo si nuestra mascota presenta alguna de estas alteraciones, ya que, de no controlarse, pueden desencadenarse problemas más graves como infecciones graves, úlceras complicadas y ceguera entre otros.',
-    ],
-  },
-  {
-    title: 'Cataratas',
-    paragraphs: [
-      'Al igual que en personas, la catarata es la opacificación del cristalino, que es una estructura en forma de lente que se encuentra en el interior del ojo; cuando esta alteración está avanzada podemos ',
-      'Cualquier animal puede presentar cataratas, aunque hay algunas especies o razas que están más predispuestas como el Yorkshire Terrier, el Cocker o el Bulldog francés.',
-      'Dependiendo del tipo de catarata, la edad de presentación cambia, aunque lo más frecuente es que se presente en individuos de edad avanzada, pueden desarrollarse en otros momentos de la vida de nuestras mascotas.',
-      'La recomendación en caso de sospecha de catarata acudir al oftalmólogo, para valorar si es operable o no y para controlar las posibles consecuencias como la ceguera y el glaucoma en el ojo en caso de que se confirme su existencia.',
-    ],
-  },
-  {
-    title: 'Atrofia de retina',
-    paragraphs: [
-      'La atrofia de retina es una manera genérica de llamar a un conjunto de enfermedades en las que la retina, que es la porción del ojo que más participa en el proceso de la visión, deja de funcionar con normalidad.',
-      'Al igual que en las otras enfermedades, diversas especies o razas tienen mayor predisposición a padecerla y dependiendo del tipo de atrofia se presentará a una edad u otra.',
-      'El síntoma principal es la pérdida de visión, que suele ser gradual y en ciertas condiciones dependiendo del tipo de atrofia.',
-      'Se recomienda acudir al oftalmólogo si se observa una disminución en la visión y si hay cambios de hábitos o comportamiento en nuestras mascotas.',
-    ],
-  },
-]
+const SYMPTOMS_TITLE = {
+  title: 'Posibles síntomas',
+  subtitle:
+    'Si tu mascota presenta alguno de estos síntomas, deberías llevarlo a tu clínica veterinaria de confianza.',
+}
 
 const SYMPTOMS = [
   'Legañas',
@@ -38,7 +14,48 @@ const SYMPTOMS = [
   'Opacificación de alguna parte del ojo',
 ]
 
+const DISEASES_TITLE = {
+  title: 'Enfermedades',
+  subtitle:
+    'Aquí puedes informarte acerca de las enfermedades oculares más comunes',
+}
+
+const DISEASES = [
+  {
+    title: 'Queratoconjuntivitis seca',
+    paragraphs: [
+      'Esta alteración es muy frecuente en ciertas razas caninas como el Yorkshire Terrier, el West Highland White Terrier y el Bulldog Francés entre otros. Se caracteriza por una disminución de la producción de uno o varios componentes de la película lagrimal.',
+      'Las mascotas que padecen esta enfermedad suelen presentar legañas, conjuntivitis recurrentes y úlceras corneales frecuentes.',
+      'Esta alteración es crónica, pero en muchas ocasiones si se diagnostica a tiempo, las mascotas que lo padecen se pueden controlar con un tratamiento y ciertos cuidados determinados.',
+      'Se recomienda acudir al oftalmólogo si nuestra mascota presenta alguna de estas alteraciones, ya que, de no controlarse, pueden desencadenarse problemas más graves como infecciones graves, úlceras complicadas y ceguera entre otros.',
+    ],
+    img: require('@/assets/images/placeholder.png'),
+  },
+  {
+    title: 'Cataratas',
+    paragraphs: [
+      'Al igual que en personas, la catarata es la opacificación del cristalino, que es una estructura en forma de lente que se encuentra en el interior del ojo; cuando esta alteración está avanzada podemos ',
+      'Cualquier animal puede presentar cataratas, aunque hay algunas especies o razas que están más predispuestas como el Yorkshire Terrier, el Cocker o el Bulldog francés.',
+      'Dependiendo del tipo de catarata, la edad de presentación cambia, aunque lo más frecuente es que se presente en individuos de edad avanzada, pueden desarrollarse en otros momentos de la vida de nuestras mascotas.',
+      'La recomendación en caso de sospecha de catarata acudir al oftalmólogo, para valorar si es operable o no y para controlar las posibles consecuencias como la ceguera y el glaucoma en el ojo en caso de que se confirme su existencia.',
+    ],
+    img: require('@/assets/images/placeholder.png'),
+  },
+  {
+    title: 'Atrofia de retina',
+    paragraphs: [
+      'La atrofia de retina es una manera genérica de llamar a un conjunto de enfermedades en las que la retina, que es la porción del ojo que más participa en el proceso de la visión, deja de funcionar con normalidad.',
+      'Al igual que en las otras enfermedades, diversas especies o razas tienen mayor predisposición a padecerla y dependiendo del tipo de atrofia se presentará a una edad u otra.',
+      'El síntoma principal es la pérdida de visión, que suele ser gradual y en ciertas condiciones dependiendo del tipo de atrofia.',
+      'Se recomienda acudir al oftalmólogo si se observa una disminución en la visión y si hay cambios de hábitos o comportamiento en nuestras mascotas.',
+    ],
+    img: require('@/assets/images/placeholder.png'),
+  },
+]
+
 module.exports = {
-  DISEASES,
+  SYMPTOMS_TITLE,
   SYMPTOMS,
+  DISEASES_TITLE,
+  DISEASES,
 }
--- src/views/Diseases.vue
+++ src/views/Diseases.vue
@@ -1,22 +1,100 @@
 <template>
-  <div>
-    <v-container fluid fill-height>
-      <v-row align="center" justify="center">
-        <v-img
-          src="@/assets/images/placeholder.png"
-          max-height="60vh"
-          contain
-          class="mb-16"
-        >
-        </v-img>
-      </v-row>
-    </v-container>
-  </div>
+  <v-container fluid fill-height class="mb-16 px-sm-5">
+    <v-row align="center" justify="center">
+      <v-img
+        src="@/assets/images/placeholder.png"
+        max-height="60vh"
+        contain
+        class="mb-16"
+      >
+      </v-img>
+    </v-row>
+
+    <!--SYMPTOMS-->
+    <v-row justify="center">
+      <v-col>
+        <v-card class="mx-1">
+          <v-card-title>
+            <p class="mx-auto text-h4 text-center font-weight-bold">
+              {{ sympTitle.title }}
+            </p>
+          </v-card-title>
+          <v-card-subtitle>
+            <p class="text-subtitle-1 text-sm-h6 text-justify font-weight-bold">
+              {{ sympTitle.subtitle }}
+            </p>
+          </v-card-subtitle>
+          <v-card-text>
+            <v-list>
+              <v-list-item v-for="(symptom, idx) in symptoms" :key="idx">
+                <v-list-item-icon>
+                  <v-icon color="accent">mdi-eye</v-icon>
+                </v-list-item-icon>
+                <span class="text-body-1"> {{ symptom }}</span>
+              </v-list-item>
+            </v-list>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <!--DISEASES-->
+    <v-row>
+      <v-col>
+        <v-card class="mx-1" color="secondary">
+          <v-card-title>
+            <p class="mx-auto text-h4 text-center font-weight-bold">
+              {{ disTitle.title }}
+            </p>
+          </v-card-title>
+          <v-card-subtitle>
+            <p class="text-subtitle-1 text-sm-h6 text-justify font-weight-bold">
+              {{ disTitle.subtitle }}
+            </p>
+          </v-card-subtitle>
+          <v-card-text class="pa-0">
+            <v-expansion-panels focusable>
+              <v-expansion-panel v-for="(item, idx) in diseases" :key="idx">
+                <v-expansion-panel-header>{{
+                  item.title
+                }}</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <v-img :src="item.img" max-height="30vh" contain> </v-img>
+                  <p
+                    v-for="(par, idx) in item.paragraphs"
+                    :key="idx"
+                    class="text-body-2 text-lg-subtitle-1"
+                  >
+                    {{ par }}
+                  </p>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import { DISEASES, SYMPTOMS } from '../helpers/diseasesStrings'
-export default {}
+import {
+  DISEASES,
+  SYMPTOMS,
+  SYMPTOMS_TITLE,
+  DISEASES_TITLE,
+} from '../helpers/diseasesStrings'
+
+export default {
+  data: function () {
+    return {
+      sympTitle: SYMPTOMS_TITLE,
+      symptoms: SYMPTOMS,
+      disTitle: DISEASES_TITLE,
+      diseases: DISEASES,
+    }
+  },
+}
 </script>
 
 <style scoped></style>
